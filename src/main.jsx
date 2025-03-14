3efc49e785fc8b08d07494ccc4e30ae9820cf19c
Fernando Farfan <fernando.farfan16@gmail.com>
2025-03-14 06:11:04 -0300

    arreglado problema de pantalla en blanco con BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
+// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // Añadir esta línea
 import App from "./App.jsx";
 import "./index.css";
 import store, { persistor } from "./redux/store";
@@ -10,7 +12,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <App />
+        <BrowserRouter> {/* Agrega esto aquí */}
+          <App />
+        </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
